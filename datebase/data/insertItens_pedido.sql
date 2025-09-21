925d623c15009961f8af15bd4314bb537b080280
Otavio2007 <otavioaredes62@gmail.com>
2025-09-21 18:24:30 -0300

    Inserção de itens dos pedidos

---
+++ datebase/data/insertItens_pedido.sql
@@ -0,0 +1,9 @@
+-- Inserir Itens dos Pedidos
+INSERT INTO Itens_Pedido (id_pedido, id_produto, quantidade, preco_unitario, desconto_item) VALUES 
+(1, 1, 1, 4999.99, 0.00),
+(1, 3, 1, 89.99, 0.00),
+(2, 2, 1, 8999.99, 0.00),
+(3, 3, 1, 89.99, 0.00),
+(3, 4, 1, 149.99, 10.00),
+(4, 5, 1, 899.99, 0.00),
+(5, 4, 1, 149.99, 0.00);
