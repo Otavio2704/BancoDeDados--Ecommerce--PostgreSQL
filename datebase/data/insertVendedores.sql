db6159e61480164ff5a9df5de15dceb142ebda88
Otavio2007 <otavioaredes62@gmail.com>
2025-09-21 18:18:06 -0300

    Inserção de vendedores

---
+++ datebase/data/insertVendedores.sql
@@ -0,0 +1,4 @@
+-- Inserir Vendedores
+INSERT INTO Vendedores (nome, email, telefone, cpf, taxa_comissao) VALUES 
+('Vendedor Silva', 'vendedor1@empresa.com', '11555666777', '11122233344', 7.50),
+('Maria Vendedora', 'maria.v@empresa.com', '11444555666', '55566677788', 6.00);
