8c4cf98d5bf023146cd37ed16918b7ec2c14e8b8
Otavio2007 <otavioaredes62@gmail.com>
2025-09-21 18:20:22 -0300

    Inserção de estoque

---
+++ datebase/data/insertEstoque.sql
@@ -0,0 +1,7 @@
+-- Inserir Estoque
+INSERT INTO Estoque (id_produto, id_vendedor, quantidade, localizacao) VALUES 
+(1, 1, 50, 'Depósito A1'),
+(2, 1, 20, 'Depósito A1'),
+(3, 2, 100, 'Depósito B2'),
+(4, 2, 75, 'Depósito B2'),
+(5, NULL, 10, 'Depósito Central');
