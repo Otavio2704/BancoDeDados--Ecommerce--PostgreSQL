4ba1dfaf41ec7f5b63d1b2a73468c339f32218dd
Otavio2007 <otavioaredes62@gmail.com>
2025-09-21 18:15:54 -0300

    Inserção de endereços

---
+++ datebase/data/insertEndereco.sql
@@ -0,0 +1,7 @@
+-- Inserir Endereços
+INSERT INTO Enderecos (id_cliente, tipo_endereco, logradouro, numero, bairro, cidade, estado, cep) VALUES 
+(1, 'Residencial', 'Rua das Flores', '123', 'Centro', 'São Paulo', 'SP', '01234567'),
+(2, 'Residencial', 'Av. Paulista', '456', 'Bela Vista', 'São Paulo', 'SP', '01310100'),
+(3, 'Comercial', 'Rua Augusta', '789', 'Consolação', 'São Paulo', 'SP', '01305100'),
+(4, 'Comercial', 'Rua Oscar Freire', '321', 'Jardins', 'São Paulo', 'SP', '01426001'),
+(5, 'Comercial', 'Av. Faria Lima', '654', 'Itaim Bibi', 'São Paulo', 'SP', '04538132');
