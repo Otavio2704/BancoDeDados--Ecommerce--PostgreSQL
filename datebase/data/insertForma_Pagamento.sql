9bbf64b663d8c5ca80c1443b1ac4f542752b8132
Otavio2007 <otavioaredes62@gmail.com>
2025-09-21 18:20:54 -0300

    Inserção de formas de pagamento

---
+++ datebase/data/insertForma_Pagamento.sql
@@ -0,0 +1,7 @@
+-- Inserir Formas de Pagamento
+INSERT INTO Formas_Pagamento (nome, descricao) VALUES 
+('Cartão de Crédito', 'Pagamento com cartão de crédito'),
+('Cartão de Débito', 'Pagamento com cartão de débito'),
+('PIX', 'Transferência instantânea PIX'),
+('Boleto Bancário', 'Pagamento via boleto bancário'),
+('Dinheiro', 'Pagamento em espécie');
