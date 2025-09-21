e47106161ab1e65d7c8f8cbbfc1b75c9f12bc64c
Otavio2007 <otavioaredes62@gmail.com>
2025-09-21 18:13:12 -0300

    Inserção de subcategorias

---
+++ datebase/data/insertSubcategoria.sql
@@ -0,0 +1,6 @@
+-- Inserir subcategorias
+INSERT INTO Categorias (nome, descricao, categoria_pai) VALUES 
+('Smartphones', 'Telefones celulares', 1),
+('Laptops', 'Computadores portáteis', 1),
+('Roupas Masculinas', 'Vestuário masculino', 2),
+('Roupas Femininas', 'Vestuário feminino', 2);
