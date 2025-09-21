80205788f33443a6482e9759e644a1c7f7998f98
Otavio2007 <otavioaredes62@gmail.com>
2025-09-21 18:12:35 -0300

    Inserção de categorias

---
+++ datebase/data/insertCategorias.sql
@@ -0,0 +1,6 @@
+INSERT INTO Categorias (nome, descricao) VALUES 
+('Eletrônicos', 'Produtos eletrônicos e tecnológicos'),
+('Roupas', 'Vestuário e acessórios'),
+('Casa e Jardim', 'Produtos para casa e jardim'),
+('Livros', 'Livros e materiais educacionais'),
+('Esportes', 'Artigos esportivos e fitness');
