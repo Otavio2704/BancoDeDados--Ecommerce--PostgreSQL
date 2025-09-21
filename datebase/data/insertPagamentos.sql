2512238d7eb0bc1e65b9c2c2764a9f5ab93567df
Otavio2007 <otavioaredes62@gmail.com>
2025-09-21 18:26:03 -0300

    Inserção de pagamentos

---
+++ datebase/data/insertPagamentos.sql
@@ -0,0 +1,7 @@
+-- Inserir Pagamentos
+INSERT INTO Pagamentos (id_pedido, id_forma_pagamento, valor_pagamento, status_pagamento) VALUES 
+(1, 1, 3000.00, 'Aprovado'),
+(1, 3, 2039.98, 'Aprovado'),
+(2, 1, 8999.99, 'Aprovado'),
+(3, 3, 229.98, 'Aprovado'),
+(5, 2, 149.99, 'Aprovado');
