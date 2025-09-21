291dc8bfd561b06094081e2aeb49aa3a24ca8def
Otavio2007 <otavioaredes62@gmail.com>
2025-09-21 18:19:48 -0300

    Inserção de dados que relacionam Fornecedores - Produtos

---
+++ datebase/data/insertFornecedores_Produtos.sql
@@ -0,0 +1,7 @@
+-- Relacionar Fornecedores com Produtos
+INSERT INTO Fornecedor_Produto (id_fornecedor, id_produto, preco_fornecimento, prazo_entrega_dias) VALUES 
+(1, 1, 4200.00, 5),
+(1, 2, 7500.00, 7),
+(2, 3, 45.00, 3),
+(2, 4, 89.00, 3),
+(3, 5, 650.00, 15);
