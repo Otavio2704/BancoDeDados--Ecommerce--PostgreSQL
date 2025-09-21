73e6a14da531a152fddd2af09f4598e97b0b012c
Otavio2007 <otavioaredes62@gmail.com>
2025-09-21 18:13:52 -0300

    Inserção de fornecedores

---
+++ datebase/data/insertFornecedores.sql
@@ -0,0 +1,5 @@
+- Inserir Fornecedores
+INSERT INTO Fornecedores (razao_social, nome_fantasia, cnpj, email, telefone, contato_responsavel) VALUES 
+('Tech Distribuidor LTDA', 'TechDist', '12345678000191', 'contato@techdist.com', '11987654321', 'João Silva'),
+('Moda Brasil S.A.', 'ModaBR', '98765432000112', 'vendas@modabr.com', '11876543210', 'Maria Santos'),
+('Casa Lar Fornecimentos', 'CasaLar', '45678912000133', 'pedidos@casalar.com', '11765432109', 'Pedro Costa');
