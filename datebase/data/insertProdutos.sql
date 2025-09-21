221cdf417e5015db6186b60c81d9df21e34ce89f
Otavio2007 <otavioaredes62@gmail.com>
2025-09-21 18:18:46 -0300

    Inserção de produtos

---
+++ datebase/data/insertProdutos.sql
@@ -0,0 +1,7 @@
+-- Inserir Produtos
+INSERT INTO Produtos (nome, descricao, preco, id_categoria, peso, dimensoes) VALUES 
+('iPhone 15', 'Smartphone Apple iPhone 15 128GB', 4999.99, 6, 0.171, '14.76 x 7.15 x 0.78 cm'),
+('MacBook Air M2', 'MacBook Air 13" com chip M2', 8999.99, 7, 1.24, '30.41 x 21.24 x 1.13 cm'),
+('Camiseta Polo', 'Camiseta Polo Masculina 100% Algodão', 89.99, 8, 0.2, 'P/M/G/GG'),
+('Vestido Floral', 'Vestido Feminino Estampa Floral', 149.99, 9, 0.3, 'P/M/G/GG'),
+('Mesa de Jantar', 'Mesa de Jantar 6 Lugares', 899.99, 3, 25.0, '160x90x75 cm');
