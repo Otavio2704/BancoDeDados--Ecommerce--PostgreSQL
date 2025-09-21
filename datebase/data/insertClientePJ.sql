236fb79f730e9b3024993bf1998adea63e6b1d9c
Otavio2007 <otavioaredes62@gmail.com>
2025-09-21 18:15:22 -0300

    Inserção de clientes PJ

---
+++ datebase/data/insertClientePJ.sql
@@ -0,0 +1,4 @@
+-- Inserir Clientes PJ
+INSERT INTO Clientes (nome, email, telefone, tipo_cliente, cnpj, razao_social, inscricao_estadual) VALUES 
+('Empresa ABC', 'contato@empresaabc.com', '1133334444', 'PJ', '11222333000144', 'ABC Comercio LTDA', '123456789'),
+('Loja XYZ', 'pedidos@lojaxyz.com', '1144445555', 'PJ', '55666777000155', 'XYZ Varejo S.A.', '987654321');
